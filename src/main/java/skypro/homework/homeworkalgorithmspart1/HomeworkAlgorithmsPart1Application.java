acc3aa92feb091e97301361941374f53a469dc8d
zemscky <aleksei.zemscky@yandex.ru>
2022-11-27 20:25:08 +0300

    Initial commit

---
+++ src/main/java/skypro/homework/homeworkalgorithmspart1/HomeworkAlgorithmsPart1Application.java
@@ -0,0 +1,13 @@
+package skypro.homework.homeworkalgorithmspart1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkAlgorithmsPart1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkAlgorithmsPart1Application.class, args);
+    }
+
+}
---
+++ src/test/java/skypro/homework/homeworkalgorithmspart1/HomeworkAlgorithmsPart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.homework.homeworkalgorithmspart1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkAlgorithmsPart1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
